bf095dc4bd99b915739d800d175f7ef5a3d42358
yash-raj10 <yashraj.se10@gmail.com>
2025-01-13 14:35:51 +0530

    sheet fn working :) hehe

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"context"
 	"database/sql"
-	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
-	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/option"
 	"google.golang.org/api/sheets/v4"
@@ -24,78 +22,14 @@ type form struct {
 	Gmail       string `json:"gmail"`
 	Description string `json:"description"`
 }
+
 type BotInfo struct {
 	adminChatID int64
 }
 
-type Credentials struct {
-	Type                string `json:"type"`
-	ProjectID           string `json:"project_id"`
-	PrivateKeyID        string `json:"private_key_id"`
-	PrivateKey          string `json:"private_key"`
-	ClientEmail         string `json:"client_email"`
-	ClientID            string `json:"client_id"`
-	AuthURI             string `json:"auth_uri"`
-	TokenURI            string `json:"token_uri"`
-	AuthProviderCertURL string `json:"auth_provider_x509_cert_url"`
-	ClientCertURL       string `json:"client_x509_cert_url"`
-}
-
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(context.Background(), tok)
-}
-
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
-}
-
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
-
 func main() {
 	botInfo := &BotInfo{}
+
 	// Bot SetUP
 	err := godotenv.Load(".env.local")
 	if err != nil {
@@ -103,7 +37,7 @@ func main() {
 	}
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TG_BOT_TOKEN"))
 	bot.Debug = true
-	//log.Printf("Authorized on account %s", bot.Self.UserName)
+	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -130,13 +64,10 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
-	client := getClient(config)
+	config.RedirectURL = "http://localhost:8080/callback"
 
-	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
-	if err != nil {
-		log.Fatalf("Unable to retrieve Sheets client: %v", err)
-	}
-	//1X5nVMQqSsYuJhxu_DAXuy0ZycKBWgSVJIeBIRZc04KA
+	// Create a channel to receive the auth client
+	clientChan := make(chan *http.Client)
 
 	//DB setup
 	DB_URL := os.Getenv("DB_URL")
@@ -154,13 +85,50 @@ func main() {
 	//Gin SetUp
 	r := gin.Default()
 
+	r.GET("/callback", func(c *gin.Context) {
+		code := c.Query("code")
+		if code == "" {
+			c.String(400, "Code not found")
+			return
+		}
+
+		tok, err := config.Exchange(ctx, code)
+		if err != nil {
+			c.String(500, "Failed to exchange token")
+			return
+		}
+
+		client := config.Client(ctx, tok)
+		clientChan <- client // Send client through channel
+		c.String(200, "Authorization successful! You can close this window.")
+	})
+
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"Update": "working"})
 	})
+
+	// Start server in a goroutine
+	go func() {
+		r.Run()
+	}()
+
+	// Start OAuth flow
+	authURL := config.AuthCodeURL("state")
+	fmt.Printf("Visit this URL to authorize: %v\n", authURL)
+
+	// Wait for client from callback
+	client := <-clientChan
+
+	// Now create sheets service
+	srv, err := sheets.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+
 	r.POST("/SendForm", sendHAndler(db, bot, botInfo, srv))
 	//r.GET("/getALL", getHandler)
 
-	r.Run()
+	select {}
 }
 
 func sendHAndler(db *sql.DB, bot *tgbotapi.BotAPI, botInfo *BotInfo, srv *sheets.Service) gin.HandlerFunc {
@@ -213,7 +181,7 @@ func sendHAndler(db *sql.DB, bot *tgbotapi.BotAPI, botInfo *BotInfo, srv *sheets
 
 		//	Sheet save
 		spreadsheetId := "1X5nVMQqSsYuJhxu_DAXuy0ZycKBWgSVJIeBIRZc04KA"
-		readRange := "Fnotifier!A2:E"
+		readRange := "Sheet1!A2:E"
 
 		values := [][]interface{}{{
 			Data.Name,
